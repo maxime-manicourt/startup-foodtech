e6ea4299bd6619acff25308ec5347aea3786eea3
maxime-manicourt <maxime.manicourt@gmail.com>
2018-08-22 04:26:07 +0200

    ajout commentaire js

--- app/assets/javascripts/components/carrousel.js
+++ app/assets/javascripts/components/carrousel.js
@@ -1,22 +1,30 @@
 $(document).ready(function () {
-
+    // toutes les 5 secondes la fonction next est lancée
     timer = window.setTimeout(next, 5000);
-
+    // a chaque clic sur la fleche de droite, la fonction next est lancee
     $('.arrow-next').click(function () {
         next();
     });
-
+    // a chaque clic sur la fleche de gauche, la fonction previous est lancee
     $('.arrow-previous').click(function() {
         previous();
     })
 
     function next() {
+        // on cree une variable qui correspond au li qui par defaut a la classe current
         var currentSlide = $('.slide__item.current');
+
+        // on cree une variable qui va chercher le prochain element li par rapport au current
         var nextSlide = currentSlide.next();
 
+        // on enleve la classe current au li qui la possedait deja
+        // on applique une methode native de jQuery, fadeOut pour animer le changement
         currentSlide.fadeOut(300).removeClass('current');
-        nextSlide.fadeIn(200).addClass('current');
 
+        // on applique la classe current au prochain element li
+        // on applique une methode native de jQuery, fadeIn pour animer le changement
+        nextSlide.fadeIn(200).addClass('current');
+        // s'il n'existe pas d'element li suivant, on revient sur le premier element li et on lui applique la classe current
         if (nextSlide.length == 0) {
             $('.slide__item').first().fadeIn(200).addClass('current');
         }
